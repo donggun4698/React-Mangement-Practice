ab59178df83de4e1981e16a502cc3f2fd29c5280
DongGeon <donggun4698@naver.com>
2019-07-11 23:21:40 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Customer from "./components/Customer";
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src ={logo} lat = "logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생"
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "이순신",
+    birthday: "930122",
+    gender: "남자",
+    job: "군인"
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "강태공",
+    birthday: "911022",
+    gender: "남자",
+    job: "어부"
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
